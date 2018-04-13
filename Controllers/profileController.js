e35f1328d78ea2cda16dbda656a5a6320d2d9e4d
Tlezip <title0088@gmail.com>
2018-04-13 16:29:20 +0700

    fix updateprofile

--- Controllers/profileController.js
+++ Controllers/profileController.js
@@ -2,7 +2,10 @@ const db = require('../db')
 
 exports.updateprofile = (req, res) => {
     const { name, department, branch, sec, email } = req.body
-    db.query("UPDATE userdetail SET Name = '" + name + "',Department = '"+ department + "',Branch = '" + branch + "',Sec = '" + sec + "',email = '" + email + "' WHERE Username = '" + req.session.username + "' AND Disabled = '0'", (err, result) => {
+    db.query("UPDATE UserDetail SET Name = '" + name + "',Department = '"+ department + "',Branch = '" + branch + "',Sec = '" + sec + "',email = '" + email + "' WHERE Username = '" + req.session.username + "' AND Disabled = '0'", (err, result) => {
+        if(err){
+            console.log(err)
+        }
         return res.json({ responseMessage: 'Update Complete'})
     })
 }
